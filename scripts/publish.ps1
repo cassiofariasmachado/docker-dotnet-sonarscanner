9503cbf61b2bb40f4b564f803356efe8a22332e5
Cássio Farias Machado <cassiofariasmachado@yahoo.com>
2020-01-09 15:00:49 -0300

    chore(scripts): add powershell publish script

---
+++ scripts/publish.ps1
@@ -0,0 +1,8 @@
+$DOTNET_SDK_VERSION=$args[0]
+$DOCKER_TAG="cassiofariasmachado/dotnet-sonarscanner:$DOTNET_SDK_VERSION"
+
+Write-Output "Stage 1: Build container"
+docker build -t $DOCKER_TAG --build-arg DOTNET_SDK_VERSION=$DOTNET_SDK_VERSION .
+
+Write-Output "Stage 2: Push docker image"
+docker push $DOCKER_TAG
